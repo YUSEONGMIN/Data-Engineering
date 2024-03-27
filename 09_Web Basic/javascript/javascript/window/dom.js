c6063cbce7e5d094706f9b8e05b9c15f2df809b8
유성민 <68213803+YUSEONGMIN@users.noreply.github.com>
2024-03-27 19:03:16 +0900

    Delete 09_Web Basic/javascript/javascript directory

--- 09_Web Basic/javascript/javascript/window/dom.js
+++
@@ -1,6 +0,0 @@
-// dom 객체의 body에는 style이 있음 -> 문서 배경 색 등
-document.body.style.backgroundColor = 'lightgreen';
-
-setTimeout(() => {
-    document.body.style.backgroundColor = "lightblue";
-}, 2000);
